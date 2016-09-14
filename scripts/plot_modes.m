0538e72af2e5057a88fb3d7b91691a358e6c3ab4
deepak <dcherian@mit.edu>
2016-09-14 15:54:41 -0400

    Update plot.
    1. Fix empty axes.
    2. Use packfig.
    3. Save hash too.

--- scripts/plot_modes.m
+++ scripts/plot_modes.m
@@ -4,31 +4,41 @@
 
 load Imode.mat
 
-figure(2); clf;
-set(gcf,'Position',[0 0 1600 900]);
+hfig = figure; clf;
+set(hfig,'Position',[0 0 1600 900]);
+hash = githash([mfilename('fullpath') '.m']);
+insertAnnotation(['plot_modes: ' hash]);
 
 xlimits = [-0.2 1.2];
 ylimits = [0 500];
-fontSize = [11 13 14];
+fontSize = [20 24 28];
 
 nlon = length(modes.lon);
 nlat = length(modes.lat);
+
+hax = packfig(nlon,nlat);
 
 for mm=1:nlon
     for nn=1:nlat 
         ind500 = find_approx(modes.zTmode,500,1);
         if isempty(modes.Imode{mm,nn}), continue; end
         subplot_index = sub2ind([nlon nlat],mm,nn);
-        figure(2);
-        subplot(nlat,nlon,subplot_index);
+
+        axes(hax(subplot_index));
         hold on
+
         % temp std
-        plot(data.tstd{mm,nn}./max(data.tstd{mm,nn}),data.depth{mm,nn},'k');% 'Color',0.5*[1 1 1]);
+        plot(data.tstd{mm,nn}./max(data.tstd{mm,nn}),data.depth{mm,nn},'k');
+
         % 0 mean flow mode
-        plot(squeeze(abs(modes.Tmode(mm,nn,:)))./max(abs(modes.Tmode(mm,nn,1:ind500))), ...
-            modes.zTmode,'r')%'Color',0.6*[1 1 1]); % 
+        plot(squeeze(abs(modes.Tmode(mm,nn,:))) ...
+             ./ max(abs(modes.Tmode(mm,nn,1:ind500))), ...
+             modes.zTmode,'r'); %'Color',0.6*[1 1 1]); %
+
         % inferred mode from TAO data
-        plot(modes.Imode{mm,nn},modes.depth{mm,nn},'bo-');
+        plot(modes.Imode{mm,nn},modes.depth{mm,nn}, 'bo-', ...
+             'LineWidth', 1);
+
         linex(0);
         xlim(xlimits); ylim(ylimits);
         set(gca,'YTick',[0 200 400]);
@@ -47,25 +57,23 @@ for mm=1:nlon
         beautify(fontSize);
         set(gca,'TickLength',[0.03 0.03]);
         box off
-        
     end
 end
 
+% remove locations without data
+hax(4).delete;
+
+hax(46).YAxis.Visible = 'off';
+hax(46).XLabel.String = '140W';
+axes(hax(46)); beautify(fontSize);
+hax(46).XAxis.Color = [1 1 1];
+hax(46).XLabel.Color = hax(45).XLabel.Color;
+
 %%
 
-% fix missing lon labels
-missing_lon = 140;
-figure(2)
-for ii = 1:length(missing_lon)
-    subplot(nlat,nlon,sub2ind([nlon nlat],find(modes.lon == missing_lon(ii)),nlat))
-    xlabel('140 W');
-    set(gca,'YTickLabel',[]);
-    beautify(fontSize);
-    set(gca,'YColor',[1 1 1]);
-    box off;
-end
-
-figure(2)
-[ax,h] = suplabel('Red = Theoretical Mode, Blue = Inferred Mode, Black = Std. of temp at depth','t');
+figure(hfig)
+[ax,h] = suplabel(['Red = Theoretical Mode, Blue = Inferred Mode, ' ...
+                   'Black = Std. of temp at depth'],'t');
+ax.YLabel.String = 'Z (m)';
 set(h,'FontSize',fontSize(3));
-export_fig('-nocrop','-r150','../images/combined.png');
+%export_fig('-nocrop','-r150','../images/combined.png');
