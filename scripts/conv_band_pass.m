788ac5685aefb1611666e0855b87add36fdc9bb4
deepak <dcherian@mit.edu>
2016-09-14 15:48:37 -0400

    Whitespace fixes.

--- scripts/conv_band_pass.m
+++ scripts/conv_band_pass.m
@@ -2,7 +2,10 @@ function [filtered] = conv_band_pass(in,windows)
     
     if size(in,1) == 1; in = in'; transpose_flag = 1; end
     
-    filtered = conv(in,ones(1,windows(1))./windows(1),'same') - conv(in,ones(1,windows(2))./windows(2),'same');
-    filtered = filtered(windows(2):end-windows(2)) - nanmean(filtered(windows(2):end-windows(2)));
+    filtered = conv(in,ones(1,windows(1))./windows(1),'same') - ...
+        conv(in,ones(1,windows(2))./windows(2),'same');
+
+    filtered = filtered(windows(2):end-windows(2)) - ...
+        nanmean(filtered(windows(2):end-windows(2)));
     
     if transpose_flag, filtered = filtered'; end
