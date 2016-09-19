df85476a79b3e1764f3c0b95b7363dac0d35d81e
deepak <dcherian@mit.edu>
2016-09-19 17:13:24 -0400

    Update freq

--- scripts/master.m
+++ scripts/master.m
@@ -1,5 +1,7 @@
 %% first figure out modes
 
+clear opt
+
 % main options
 opt.debug = 0; % debugging spectrum plots
 opt.filter_temp = 1; % filter temperature also?
@@ -7,7 +9,7 @@ opt.name = 'bc2m1';
 opt.n_modes = 3; % number of modes to calculate
 opt.n_mode = 2; % which theoretical mode am I looking for?
 
-opt.filt.cutoff = [5 9]; % (days) band pass filter windows
+opt.filt.cutoff = 2./[0.135 0.155]; % (days) band pass filter windows
 opt.filt.window = 'rect'; % window shape
 opt.filt.halfdef = 'power'; % how is filt.N defined?
 opt.filt.N = NaN; % will be set based on cutoff later.
