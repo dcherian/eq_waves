1f33a17b31f93a9b1a459c81a3194c11a8338262
deepak <dcherian@mit.edu>
2016-09-21 13:51:49 -0400

    master.m does multiple waves now.

--- scripts/master.m
+++ scripts/master.m
@@ -5,18 +5,39 @@ clear opt
 % main options
 opt.debug = 0; % debugging spectrum plots
 opt.filter_temp = 1; % filter temperature also?
-opt.name = 'bc2m1';
 opt.n_modes = 3; % number of modes to calculate
-opt.n_mode = 2; % which theoretical mode am I looking for?
 
-opt.filt.cutoff = 2./[0.135 0.155]; % (days) band pass filter windows
-opt.filt.window = 'rect'; % window shape
+opt.filt.window = 'gauss'; % window shape
 opt.filt.halfdef = 'power'; % how is filt.N defined?
 opt.filt.N = NaN; % will be set based on cutoff later.
 opt.filt.debugflag = 0; % debugging spectrum plots in BandPass()
 
+%% bc2m1
+
+opt.name = 'bc2m1';
+opt.n_mode = 2; % which theoretical mode am I looking for?
+opt.filt.cutoff = 2./[0.135 0.155]; % (days) band pass filter windows
 InferModeShape(opt);
+PlotModeMap(opt.name);
 
-%% Plot bc2m1 shape
+export_fig -r300 images/09-21-bc2m1.png
 
-PlotModeMap('bc2m1');
+%% bc2m2
+
+opt.name = 'bc2m2';
+opt.n_mode = 2;  % which theoretical mode am I looking for?
+opt.filt.cutoff = 2./[0.19 0.21]; % (days) band pass filter windows
+InferModeShape(opt);
+PlotModeMap(opt.name);
+
+export_fig -r300 images/09-21-bc2m2.png
+
+%% bc1m1
+
+opt.name = 'bc1m1';
+opt.n_mode = 1;  % which theoretical mode am I looking for?
+opt.filt.cutoff = 2./[0.19 0.21]; % (days) band pass filter windows
+InferModeShape(opt);
+PlotModeMap(opt.name);
+
+export_fig -r300 images/09-21-bc1m1.png
