5fd8788597f99de70cef810efe2aa8625073efb3
deepak <dcherian@mit.edu>
2016-09-23 10:44:33 -0400

    Update PlotSpectrum to use Tom's code.
    Spectrum is estimated for longest portion of input time series without gaps.

--- scripts/PlotSpectrum.m
+++ scripts/PlotSpectrum.m
@@ -3,8 +3,22 @@ function [hplt] = PlotSpectrum(in, hax)
     if ~exist('hax', 'var'), hax = gca; end
     if size(in, 1) == 1, in = in'; end
 
+    data = ~isnan(in);
+    cc = bwconncomp(data);
+
+    maxdataidx = 1;
+    for ii=2:cc.NumObjects
+        if length(cc.PixelIdxList{ii}) > ...
+                length(cc.PixelIdxList{maxdataidx})
+            maxdataidx = ii;
+        end
+    end
+
     axes(hax); hold on;
-    [F,S] = mspec(in, [], 'cyclic');
+    [S, F] = spectrum_band_avg(in(cc.PixelIdxList{maxdataidx}), ...
+                              1, 5, 'hann', 0);
+    %[F,S] = mspec(in, [], 'cyclic');
+
     hplt = plot(F,S);
     hax.XScale = 'log';
     hax.YScale = 'log';
