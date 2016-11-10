d2a1155da9dcaaf8dadbaf6bd57dafdea2e22294
deepak <dpak.cherian@gmail.com>
2016-11-10 15:41:12 -0500

    Revert "rough fix" normalization.
    Gah, if I go with WTLS I'll fix them at individual locations. Works fine
    at most places.

--- scripts/InferModeShape.m
+++ scripts/InferModeShape.m
@@ -182,10 +182,10 @@ function [norm] = findNormalization(shape)
 
     [norm,imax] = nanmax(shape);
 
-    if imax > 16
-        % max is too deep, generally happens with WTLS
-        [norm,imax] = nanmax(shape(1:imax-1));
-    end
+    % if imax > 17
+    %     % max is too deep, generally happens with WTLS
+    %     [norm,imax] = nanmax(shape(1:imax-1));
+    % end
 
     % Normalize so that mode shape is 1 at water depth
     % closest to where the theoretical mode maximum is.
