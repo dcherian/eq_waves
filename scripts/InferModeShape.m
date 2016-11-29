9c67fc0fcf8d646447c1cfcdd37fe31e39ed18a1
deepak <dpak.cherian@gmail.com>
2016-11-29 11:23:02 -0500

    Break out TAO/TRITON data reading from InferModeShape.

--- scripts/InferModeShape.m
+++ scripts/InferModeShape.m
@@ -1,30 +1,12 @@
 % Get mode shape from data by band pass filtering around expected frequency
-
-% This is how the script works for each point in the TAO array
-% 1) Read TAO temp data
-% 2) Read dynamic height data and band pass filter around given
-%    band
-% 3) At each depth, band pass filter interpolated temp data, find missing
-%    data in both DHT & temp, and remove them. Then, do the inversion and
-%    calculate "inferred mode" (Imode) again at each depth.
-% 4) Normalize by *max amplitude* and save data to Imode.mat.
-
-function [modes] = InferModeShape(opt, lonrange, latrange)
-
-  %% Data & Parameters
+function [modes] = InferModeShape(opt, data, lonrange, latrange)
 
   ticstart = tic;
-  datadir = '../data/';
 
-  modes.lat = [8 5 2 0 -2 -5 -8]; % N
-  % modes.lon = [95 110 125 140 137 147 155 156 165 170 180]; % W
-  % edited lon values to make neater subplots ...
-  % (some lons dont have enough data)
-  % values in +ve East.
-  modes.lon = -1 * fliplr([95 110 125 140 155 170 180 -165 -156 -147 -137]);
+  if ~exist('data', 'var'), error('Provide data.'); end
 
-  nlon = length(modes.lon);
-  nlat = length(modes.lat);
+  nlon = length(data.lon);
+  nlat = length(data.lat);
 
   if ~exist('lonrange', 'var')
       lonrange = 1:nlon;
@@ -38,99 +20,63 @@ function [modes] = InferModeShape(opt, lonrange, latrange)
   end
   load('flat-bot-modes');
 
-  %% Iterate!
-  clear data
+  modes.lon = data.lon;
+  modes.lat = data.lat;
+  modes.depth = data.depth;
+
   for mm=lonrange
       for nn=latrange
-
-          if modes.lon(mm) > 0
-              lonstr = 'e';
-          else
-              lonstr = 'w';
+          if isempty(data.dht{mm,nn}) | isempty(data.T{mm,nn})
+              continue;
           end
 
-          if modes.lat(nn) < 0
-              latstr = 's';
-          else
-              latstr = 'n';
-          end
-
-          % TAO filenames
-          fnamet = [datadir 'temp/t',   num2str(abs(modes.lat(nn))), ...
-                    latstr,num2str(abs(modes.lon(mm))),lonstr,'_dy.cdf'];
-          fnameh = [datadir 'dynht/dyn',num2str(abs(modes.lat(nn))), ...
-                    latstr,num2str(abs(modes.lon(mm))),lonstr,'_dy.cdf'];
-
-          % make sure observations exist before continuing
-          if ~exist(fnamet,'file'), continue; end
-          if ~exist(fnameh,'file'), continue; end
-
-          fprintf(['TAO at (%.1f %s, %.1f %s)\n'], ...
-                  abs(modes.lon(mm)), upper(lonstr), ...
-                  abs(modes.lat(nn)), upper(latstr));
-
-          % read in TAO measurements
-          tao.T = double(addnan(squeeze(ncread(fnamet,'T_20')),100));
-          modes.depth{mm,nn} = squeeze(ncread(fnamet,'depth'));
-          tao.dht = ...
-              double(addnan(squeeze(ncread(fnameh,'DYN_13')),1000))';
-          tao.dht = tao.dht - nanmean(tao.dht);
-
-          % Read in time vectors for both variables
-          tao.timedht = ncread(fnameh, 'time');
-          tao.timetemp = ncread(fnamet, 'time');
-
-          % Bandpass filter dynamic height
-          dhtfilt = BandPass(tao.dht, opt.filt);
+          dht = data.dht{mm,nn} - nanmean(data.dht{mm,nn});
+          % First do Bandpass filtering
+          dhtfilt = BandPass(dht, opt.filt);
 
           if opt.debug
               opt.hdbg = figure;
               hdbgax1 = subplot(211);
-              PlotSpectrum(tao.dht);
+              PlotSpectrum(data.dht);
               PlotSpectrum(dhtfilt);
               linex(1./opt.windows);
+          end
+
+          % iterate over depths and filter temperature
+          clear Tfilt
+          for ii = 1:length(modes.depth{mm,nn})
+              if opt.filter_temp
+                  % band pass filter temperature data
+                  Tfilt(ii,:) = BandPass(data.T{mm,nn}(ii,:), opt.filt);
+              else
+                  Tfilt(ii,:) = data.T{mm,nn}(ii,:);
+              end
+
+              if opt.debug
+                  axes(hdbgax1);
+                  PlotSpectrum(cut_nan(data.T(ii,:)));
+                  PlotSpectrum(cut_nan(Tfilt(ii,:)));
+              end
           end
 
           % Make sure I'm using same time interval for both variables
           % Needed because dynamic height is not available at all
           % time points with temperature measurement
-          start = find(tao.timetemp == tao.timedht(1));
-          stop = find(tao.timetemp == tao.timedht(end));
-          range = start:stop;
-          assert(all(tao.timetemp(range) == tao.timedht));
+          range = findCommonTimeRange(data.timedht{mm,nn}, ...
+                                      data.timetemp{mm,nn});
 
-          % nans
+          % Initialize to nans
           infer_mode = nan(size(modes.depth{mm,nn}));
-          infer_mode_error = nan(size(modes.depth{mm,nn}));
-
+          infer_mode_error = infer_mode;
           dof = infer_mode;
           corrcoeff = infer_mode;
           Tstd = infer_mode;
-
-          % iterate over depths and regress at each
-          clear Tvec Tfilt Tstd
-          for ii = 1:length(modes.depth{mm,nn})
-              if opt.filter_temp
-                  % band pass filter temperature data
-                  Tfilt(ii,:) = BandPass(tao.T(ii,:), opt.filt);
-              else
-                  Tfilt(ii,:) = tao.T(ii,:);
-              end
-
-              if opt.debug
-                  axes(hdbgax1);
-                  PlotSpectrum(cut_nan(tao.T(ii,:)));
-                  PlotSpectrum(cut_nan(Tfilt(ii,:)));
-              end
-
-              % save temperature series for reference and calculate std dev
-              data.Tfilt{mm,nn,ii} = Tfilt(ii,:);
-          end
 
           Tstd = nanstd(Tfilt(:,range)')';
           [infer_mode, infer_mode_error, corrcoeff, dof] ...
               = DoRegression(dhtfilt, Tfilt(:, range), opt);
 
+          % normalize mode shapes
           imnorm = findNormalization(infer_mode(:,1));
           modes.InferredModeOLS{mm,nn} = infer_mode(:,1)./imnorm;
           modes.InferredModeErrorOLS{mm,nn} = ...
@@ -153,7 +99,7 @@ function [modes] = InferModeShape(opt, lonrange, latrange)
           modes.dof{mm,nn} = dof;
           modes.corr{mm,nn} = corrcoeff;
 
-          modes.timedht{mm,nn} = tao.timedht;
+          modes.timedht{mm,nn} = data.timedht;
           modes.dhtfilt{mm,nn} = dhtfilt;
           modes.Tstd{mm,nn} = Tstd;
       end
@@ -168,13 +114,23 @@ function [modes] = InferModeShape(opt, lonrange, latrange)
                   'dhtfilt = band passed dynamic height | ' ...
                   'Tstd = standard dev of temp time series at depth'];
 
-  % don't overwrite if inferring at all locations
+  % don't overwrite if *not* inferring at all locations
   if isequal(lonrange, 1:nlon) & isequal(latrange, 1:nlat)
       hash = githash([mfilename('fullpath') '.m']);
+      disp('Saving mode structures');
       save([opt.name '-' opt.filt.window '.mat'], ...
-           'modes', 'data', 'opt', 'hash');
+           'modes', 'opt', 'hash');
   end
   toc(ticstart);
+end
+
+function [range] = findCommonTimeRange(timedht, timetemp)
+
+    start = find(timetemp == timedht(1));
+    stop = find(timetemp == timedht(end));
+    range = start:stop;
+
+    assert(all(timetemp(range) == timedht));
 end
 
 function [norm] = findNormalization(shape)
---
+++ scripts/ReadTaoTriton.m
@@ -0,0 +1,68 @@
+% Reads TAO/TRITON data and returns structure tao.
+function [tao] = ReadTaoTriton(lonrange, latrange)
+
+    ticstart = tic;
+    disp('Reading TAO/TRITON data');
+
+    datadir = '../data/';
+
+    tao.lat = [8 5 2 0 -2 -5 -8];
+    % modes.lon = [95 110 125 140 137 147 155 156 165 170 180]; % W
+    % edited lon values to make neater subplots ...
+    % (some lons dont have enough data)
+    % values in +ve East.
+    tao.lon = -1 * fliplr([95 110 125 140 155 170 180 ...
+                        -165 -156 -147 -137]);
+
+    nlon = length(tao.lon);
+    nlat = length(tao.lat);
+
+    if ~exist('lonrange', 'var')
+        lonrange = 1:nlon;
+    end
+    if ~exist('latrange', 'var')
+        latrange = 1:nlat;
+    end
+
+    for mm=lonrange
+        for nn=latrange
+            if tao.lon(mm) > 0
+                lonstr = 'e';
+            else
+                lonstr = 'w';
+            end
+
+            if tao.lat(nn) < 0
+                latstr = 's';
+            else
+                latstr = 'n';
+            end
+
+            % TAO filenames
+            fnamet = [datadir 'temp/t',   num2str(abs(tao.lat(nn))), ...
+                      latstr,num2str(abs(tao.lon(mm))),lonstr,'_dy.cdf'];
+            fnameh = [datadir 'dynht/dyn',num2str(abs(tao.lat(nn))), ...
+                      latstr,num2str(abs(tao.lon(mm))),lonstr,'_dy.cdf'];
+
+            % make sure observations exist before continuing
+            if ~exist(fnamet,'file'), continue; end
+            if ~exist(fnameh,'file'), continue; end
+
+            fprintf(['TAO/TRITON at (%.1f %s, %.1f %s)\n'], ...
+                    abs(tao.lon(mm)), upper(lonstr), ...
+                    abs(tao.lat(nn)), upper(latstr));
+
+            % read in TAO measurements
+            tao.T{mm,nn} = double(addnan(squeeze(ncread(fnamet,'T_20')),100));
+            tao.depth{mm,nn} = squeeze(ncread(fnamet,'depth'));
+            tao.dht{mm,nn} = ...
+                double(addnan(squeeze(ncread(fnameh,'DYN_13')),1000))';
+
+            % Read in time vectors for both variables
+            tao.timedht{mm,nn} = ncread(fnameh, 'time');
+            tao.timetemp{mm,nn} = ncread(fnamet, 'time');
+        end
+    end
+
+    toc(ticstart);
+end
