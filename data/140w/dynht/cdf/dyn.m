1a52f266b583778212de07aea9795c98d76e3c86
deepak <dpak.cherian@gmail.com>
2017-03-23 14:27:51 -0600

    Delete some old files.

--- data/140w/dynht/cdf/dyn.m
+++
@@ -1,6 +0,0 @@
-files = ls('*.cdf');
-
-for i=1:size(files,1)
-    [vars atts dims] = ncdfread(files(i,:));
-    dyn(i,:) = squeeze(vars.DYN_13);
-end
--- data/140w/temp/cdf/temp.m
+++
@@ -1,6 +0,0 @@
-files = ls('*.cdf');
-
-for i=1:size(files,1)
-    [vars atts dims] = ncdfread(files(i,:));
-    tmp(i,:,:) = squeeze(vars.T_20);
-end
