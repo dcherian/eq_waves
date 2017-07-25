e109c964939873460af9cd4406a98de96a2e7d15
deepak <dpak.cherian@gmail.com>
2017-07-25 13:43:06 -0600

    minor updates.

--- scripts/master.m
+++ scripts/master.m
@@ -4,18 +4,18 @@ plotopt.ploterr = 1;
 plotopt.nmode = [1 2];
 
 tao = ReadTaoTriton;
-% SaveNullBoundsForTao(tao, opt);
-opt.numMC = 1500;
+SaveNullBoundsForTao(tao, opt);
+% opt.numMC = 5000;
 InferModeShape(opt, tao);
-RunTests(opt);
+% RunTests(opt);
 
 [hax, supax] = PlotModeMap(plotopt);
 supax.Title.Visible = 'off';
-savepdf('images/02-28-bc2m1.pdf');
+savepdf('images/07-27-bc2m1.pdf');
 
 [hax, supax] = PlotModeMap(plotopt, 3:11, 2:6);
 supax.Title.Visible = 'off';
 axes(hax(1)); legend('off');
-savepdf('images/02-28-bc2m1-zoom.pdf');
+savepdf('images/07-27-bc2m1-zoom.pdf');
 
 % export_fig -transparent -opengl -r150  -p0.005 -nofontswap -depsc images/01-08-bc2m1.png
